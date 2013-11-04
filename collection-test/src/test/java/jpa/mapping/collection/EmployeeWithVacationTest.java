efd31b4d4920d5611325963daa3c491c2f656acc
An Tran <tranminhan.my@gmail.com>
2013-11-04 17:17:56 +0700

    test element collection

--- collection-test/src/test/java/jpa/mapping/collection/EmployeeWithVacationTest.java
+++ collection-test/src/test/java/jpa/mapping/collection/EmployeeWithVacationTest.java
@@ -1,6 +1,11 @@
 package jpa.mapping.collection;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+
 import jpa.mapping.basic.PersistenceTest;
 
 import org.junit.Test;
@@ -15,13 +20,13 @@ public class EmployeeWithVacationTest extends PersistenceTest {
         employee.addNickname("nick1");
         employee.addNickname("nick2");
         employee.addNickname("nick3");
-        
+
         employee.addPhone("123");
-        
-        // employee.bookVacation(new VacationEntry());
-        // employee.bookVacation(new VacationEntry(new Date(), 1));
-        // employee.bookVacation(new VacationEntry(Calendar.getInstance(), 2));
-        // employee.bookVacation(new VacationEntry(Calendar.getInstance(), 3));
+
+        employee.bookVacation(new VacationEntry());
+        employee.bookVacation(new VacationEntry(new Date(), 1));
+        employee.bookVacation(new VacationEntry(new Date(), 2));
+        employee.bookVacation(new VacationEntry(new Date(), 3));
 
         entityManager.persist(employee);
         entityManager.getTransaction().commit();
@@ -29,6 +34,9 @@ public class EmployeeWithVacationTest extends PersistenceTest {
         System.out.println(employee.getId());
         assertNotNull(employee.getId());
 
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+        EmployeeWithVacation newEmployee = entityManager2.find(EmployeeWithVacation.class, employee.getId());
+        assertNotNull(newEmployee);
+        assertEquals(4, newEmployee.getVacationBookings().size());
     }
-
 }
